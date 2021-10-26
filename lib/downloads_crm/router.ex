e2e62248997cf51c70ad6c24c1a8c30245a75842
e.famiev <famiev91@gmail.com>
2021-10-27 01:33:26 +0700

    Add create project route

--- lib/downloads_crm/router.ex
+++ lib/downloads_crm/router.ex
@@ -1,8 +1,10 @@
 defmodule DownloadsCrm.Router do
   use Plug.Router
 
+  alias DownloadsCrm.Repo
+  alias DownloadsCrm.Storage.Pg.Schema.Project
+
   plug(:match)
-  plug(:dispatch)
 
   plug(Plug.Parsers,
     parsers: [:json],
@@ -10,9 +12,35 @@ defmodule DownloadsCrm.Router do
     json_decoder: Jason
   )
 
+  plug(:dispatch)
+
   get "/projects" do
     send_resp(conn, 200, "ok")
   end
 
+  post "/projects" do
+    with %{valid?: true} = changeset <- Project.changeset(%Project{}, conn.params),
+         {:ok, project} <- Repo.insert(changeset) do
+      render_json(conn, project)
+    else
+      %Ecto.Changeset{errors: errors} = res ->
+        send_resp(conn, 400, format_errors(errors))
+
+      error ->
+        IO.inspect(error, label: "Unexpected error")
+        send_resp(conn, 500, "Unexpected error")
+    end
+  end
+
   match(_, do: send_resp(conn, 404, "Oops!"))
+
+  defp format_errors(errors) do
+    errors
+    |> Enum.map(fn {attr_name, {msg, _opts}} -> "#{attr_name}: #{msg}" end)
+  end
+
+  defp render_json(%{status: status} = conn, data) do
+    body = Jason.encode!(data)
+    send_resp(conn, status || 200, body)
+  end
 end
--- lib/downloads_crm/storage/pg/schema/project.ex
+++ lib/downloads_crm/storage/pg/schema/project.ex
@@ -1,11 +1,20 @@
 defmodule DownloadsCrm.Storage.Pg.Schema.Project do
   use Ecto.Schema
 
+  import Ecto.Changeset
+
+  @derive {Jason.Encoder, except: [:__meta__, :__struct__, :tasks]}
   schema "projects" do
     field(:uuid, Ecto.UUID, autogenerate: true)
     field(:name, :string)
     field(:description, :string)
     field(:price, :float)
     has_many(:tasks, DownloadsCrm.Storage.Pg.Schema.Task)
+  end
+
+  def changeset(project, attrs \\ %{}) do
+    project
+    |> cast(attrs, [:name, :description, :price])
+    |> validate_required([:name, :price])
   end
 end
